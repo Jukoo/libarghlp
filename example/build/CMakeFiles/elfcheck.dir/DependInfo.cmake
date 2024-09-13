ab807397a51dc05265f462c31066f4e0ccf4e162
jukoo <jUmarB@protonmail.com>
2024-09-13 16:16:46 +0000

    add example

---
+++ example/build/CMakeFiles/elfcheck.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/umar/Desktop/playground/libargp/example/elfcheck.c" "CMakeFiles/elfcheck.dir/elfcheck.c.o" "gcc" "CMakeFiles/elfcheck.dir/elfcheck.c.o.d"
+  "/home/umar/Desktop/playground/libargp/example/main.c" "CMakeFiles/elfcheck.dir/main.c.o" "gcc" "CMakeFiles/elfcheck.dir/main.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ example/build/CMakeFiles/elfcheck.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for elfcheck.
+# This may be replaced when dependencies are built.
